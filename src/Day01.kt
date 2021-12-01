865d3e71865f004216dc1c8a095690e730cf6b12
Eutrix <63642309+Eutrix@users.noreply.github.com>
2021-12-01 06:36:37 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var a = 0
+        for (i in 1 until input.size) {
+            if (input[i] > input[i-1]) {
+                a += 1
+            }
+        }
+        return a
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var b = 0
+        for (i in 1 until input.size - 2) {
+            if (input.slice(i..i+2).sum() > input.slice(i-1..i+1).sum()) {
+                b += 1
+            }
+        }
+        return b
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
